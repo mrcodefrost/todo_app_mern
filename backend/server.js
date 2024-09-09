177e4e1f4b3bb08ffffcf497f987a23a292aec42
mrcodefrost <mr.codefrost@gmail.com>
2024-09-09 17:51:42 +0530

    backend complete, frontend TBD

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const routes = require("./routes/ToDoRoute");
+require("dotenv").config();
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(() => {
+    console.log("MongoDB connected");
+})
+.catch((err) => {
+    console.log(err);
+});
+
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+     
+    Weird
+
+    </div>
+  );
+}
+
+export default App;
